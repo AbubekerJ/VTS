03dc939ccd7021ff58acf9122b6e93de4782da7e
AbubekerJ <abubeker.jemal03@gmail.com>
2025-02-08 18:16:58 +0000

    Add SessionProvider to AllProvider for authentication context

--- src/app/config/all-provider.tsx
+++ src/app/config/all-provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react"
 
 export const queryClient = new QueryClient();
 
@@ -9,8 +10,12 @@ type props = {
 
 const AllProvider = ({ children }: props) => {
   return (
+    <SessionProvider >
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </SessionProvider>
   );
 };
 
 export default AllProvider;
+
+
